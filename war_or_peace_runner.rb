e258aebe8b0d0b8dbacea9e2bfec31c866c871c7
Garret Wolf <garretwolf26@gmail.com>
2020-07-04 20:46:27 -0400

    Create start method

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -83,4 +83,11 @@ class NewGame
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
   end
+
+  def start
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are Megan and Aurora."
+    p "Type 'GO' to start the game!"
+    p "------------------------------------------------------------------""
+  end
 end
