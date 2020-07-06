127552006e65a9470bb8b35871e671fe7afe313c
Garret Wolf <65677113+garretwolf@users.noreply.github.com>
2020-07-05 22:59:06 -0400

    Update new_game.rb

--- lib/new_game.rb
+++ lib/new_game.rb
@@ -1,17 +1,17 @@
 
-require './lib/deck'
-require './lib/card'
-require './lib/player'
-require './lib/turn'
+# require './lib/deck'
+# require './lib/card'
+# require './lib/player'
+# require './lib/turn'
 
-class NewGame
+# class NewGame
 
-  def start
-    p "Welcome to War! (or Peace) This game will be played with 52 cards."
-    p "The players today are Megan and Aurora."
-    p "Type 'GO' to start the game!"
-    p "------------------------------------------------------------------"
+#   def start
+#     p "Welcome to War! (or Peace) This game will be played with 52 cards."
+#     p "The players today are Megan and Aurora."
+#     p "Type 'GO' to start the game!"
+#     p "------------------------------------------------------------------"
 
-    start_game = gets.chomp
-  end
-end
+#     start_game = gets.chomp
+#   end
+# end
